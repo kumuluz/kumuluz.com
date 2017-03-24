681c783b3579b7fa9787a8d6f75238f0a00e8cd9
Aleks Drevenšek <aleks.drevensek@sunesis.si>
2017-03-24 13:02:59 +0100

    kumuluz.com initial push

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
-$(document).ready(function(){
-    // One page navigation
-    $('.nav').singlePageNav({
-        offset: $('.navbar').outerHeight()
+function translate(lang) {
+    jQuery.i18n.properties({
+        name: 'Messages',
+        path: 'lang/',
+        language: lang,
+        mode: 'both',
+        async: true,
+        callback: function() {
+            $("[data-i18n]").each(function(index, e) {
+                var translation = $(e).attr("data-i18n");
+                $(e).html(jQuery.i18n.prop(translation));
+            });
+        }
+    });
+}
+
+function populateCostumers(data) {
+    for (var customer of data) {
+        $('.clients').append(`
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 block" style="background-image:url(${customer.logo});">
+            <span class="animated fadeIn">
+                <div>${customer.name}</div>
+            </span>
+        </div>
+        `);
+    }
+}
+
+$(document).ready(function() {
+    $('.banner').css('height', $(window).innerHeight());
+
+    $.getJSON("customers.json", function(data) {
+        populateCostumers(data);
     });
 
-    // Scroll to top
-    $('.smooth-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                  scrollTop: target.offset().top
-                }, 1000);
-                return false;
-              }
-            }
-    });
+
+    translate("en");
 });
